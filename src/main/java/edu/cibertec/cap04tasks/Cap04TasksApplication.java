640f4797a14795cf05c96c747b5d4b3b4dbbb616
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-01 12:05:17 -0500

    initial commit

---
+++ src/main/java/edu/cibertec/cap04tasks/Cap04TasksApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.cap04tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap04TasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cap04TasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/cibertec/cap04tasks/Cap04TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.cap04tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap04TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
