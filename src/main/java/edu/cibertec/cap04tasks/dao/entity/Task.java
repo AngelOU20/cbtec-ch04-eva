5d9940a847613c52235ae74572cb936a7f49ab87
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-02 19:02:00 -0500

    renamed TaskEntity to Task

--- src/main/java/edu/cibertec/cap04tasks/dao/entity/Task.java
+++ src/main/java/edu/cibertec/cap04tasks/dao/entity/Task.java
@@ -1,6 +1,7 @@
 package edu.cibertec.cap04tasks.dao.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,23 +9,27 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "task")
-public class TaskEntity {
+public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Size(min = 1, max = 50, message = "El titulo no es valido")
+    @Size(min = 3, max = 50, message = "El titulo no es valido")
+    @NotNull
+    @NotBlank
     @Column(name = "title")
     private String title;
 
+    @Size(max = 250)
     @Column(name = "description")
     private String description;
 
     @NotNull
+    @NotBlank
     @Column(name = "status")
     private String status;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
-    private UserEntity user;
+    private User user;
 }
